d43e89f33a1ea234cfab666288bbc90939c44362
vtcong2299 <vtcong2299@gmail.com>
2024-08-12 16:42:12 +0700

    Commit 1

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace KiemTraSoNguyenTo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            
+            int i;
+            
+            Console.Write("Cac so nguyen to be hon 100 la: ");
+            for(num=0;num<=100;num++)
+            {
+                int count=0;
+                if (num>=2)
+                {
+                    for (i=2;i<num;i++)
+                    {
+                        if(num%i==0)
+                        {
+                            count++;
+                        }
+                    }
+                    if(count==0)
+                    {
+                        Console.Write("{0,3}",num);
+                    }
+                }
+                
+            }
+            
+           
+        }
+    }
+}
